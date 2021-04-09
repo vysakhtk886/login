9d23ba1995ae744833f0eedb70707d3b275277df
vysakhtk886 <vysakhtk886@gmail.com>
2021-04-09 18:47:31 +0530

    first commit

--- newdjango_pro/newdjango_pro/settings.py
+++ newdjango_pro/newdjango_pro/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'newdjango_pro.urls'
